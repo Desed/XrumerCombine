ebc50096c5501fa9221b62e9945c7b6a976c8aed
Desed <titan.wow@yandex.ru>
2020-02-20 22:54:41 +0300

    Update main_config.php

--- main_config.php
+++ main_config.php
@@ -10,27 +10,26 @@ $mysql_conf = array
 				'db'      => 'xrumer',
 				'port'      => 3306);
 
+
+$debug = false;
+$url_success = 'http://login.wowcrazy.ru:81/Success.txt';
 // Основные параметры
 $password = 'password124'; //ваш пароль от зареганых акков в проекте
-$xrumer_report = 'E:\Xrumer\Xrum19\Logs\pbn_sites\Posting.2020.01\Success.txt'; // Указываем путь фаила "Успешные" от Xrumer
+$xrumer_report = './success.txt'; // Указываем путь фаила "Успешные" от Xrumer
 $xrumer_new_report = './temp.txt.bak'; // не трогать.
 $str_url = implode("	", file('./check_url.txt', FILE_IGNORE_NEW_LINES)); // фаил с ссылками которые будем искать
 
-$threads = '1'; // потоков (больше 6 потоков не советую ставить)
-$threads_timeout = 8000; // Максимальное время ответа сайта в мс.
+$threads = '1'; // не трогать
+$threads_timeout = 6000; // Максимальное время ответа сайта в мс.
+$limit_pageSize = 100000; //лимит html страницы 100кб.
+
 
 //Яндекс ИКС
 $key = '12b01b30186dccae6e2e54486d4d3968'; // если xevil на внешнем адресе, создаем уникальный key
 $xrumer_host = '127.0.0.1:80'; // Адрес Xevil , по умолчанию 127.0.0.1:80 (Также в настройках Xevil укажите Anigate)
 
-$thread = '1'; // потоки на решение иконок Яндекс Икс
+$thread = '1'; // не трогать
 $limit = '100';
 
 $db = new safeMysql($mysql_conf);
 $mcurl = new mcurl();
-
-
-?>
-
-
-
